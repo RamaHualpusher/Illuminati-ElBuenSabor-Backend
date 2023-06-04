7a75392c9b5a6928cc665ae338960cc0adc208a6
RamaHualpusher <ramiro.hualpa@alumnos.frm.utn.edu.ar>
2023-06-03 23:37:02 -0300

    DTOs Mappers - completando

--- src/main/java/com/illuminati/ebs/mapper/ArticuloMapper.java
+++ src/main/java/com/illuminati/ebs/mapper/ArticuloMapper.java
@@ -2,10 +2,31 @@ package com.illuminati.ebs.mapper;
 
 import com.illuminati.ebs.dto.ArticuloDto;
 import com.illuminati.ebs.entity.Articulo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
 
 @Component
 public class ArticuloMapper implements GenericMapper<ArticuloDto, Articulo> {
+
+    @Autowired
+    private RubroMapper rubroMapper;
+
+    @Autowired
+    private UnidadMedidaMapper unidadMedidaMapper;
+
+    @Autowired
+    private DetallePedidoMapper detallePedidoMapper;
+
+    @Autowired
+    private ProductoBebidaCostoMapper productoBebidaCostoMapper;
+
+    @Autowired
+    private ProductoBebidaVentaMapper productoBebidaVentaMapper;
+
+    @Autowired
+    private ProductoBebidaStockActualMapper productoBebidaStockActualMapper;
 
     @Override
     public Articulo toEntity(ArticuloDto dto) {
@@ -17,7 +38,16 @@ public class ArticuloMapper implements GenericMapper<ArticuloDto, Articulo> {
         entity.setStockMinimo(dto.getStockMinimo());
         entity.setStockActual(dto.getStockActual());
         entity.setEsBebida(dto.getEsBebida());
-        // Asigna los valores restantes...
+
+        // Convertir idRubro y idUnidadMedida a entidades correspondientes
+        entity.setRubro(rubroMapper.toEntity(dto.getIdRubro()));
+        entity.setUnidadMedida(unidadMedidaMapper.toEntity(dto.getIdUnidadMedida()));
+
+        // Convertir las listas de IDs a listas de entidades anidadas
+        entity.setDetallesPedido(detallePedidoMapper.toEntityList(dto.getDetallesPedidoIds()));
+        entity.setProductosBebidasCosto(productoBebidaCostoMapper.toEntityList(dto.getProductosBebidasCostoIds()));
+        entity.setProductosBebidasVenta(productoBebidaVentaMapper.toEntityList(dto.getProductosBebidasVentaIds()));
+        entity.setProductosBebidasStockActual(productoBebidaStockActualMapper.toEntityList(dto.getProductosBebidasStockActualIds()));
 
         return entity;
     }
@@ -32,7 +62,16 @@ public class ArticuloMapper implements GenericMapper<ArticuloDto, Articulo> {
         dto.setStockMinimo(entity.getStockMinimo());
         dto.setStockActual(entity.getStockActual());
         dto.setEsBebida(entity.getEsBebida());
-        // Asigna los valores restantes...
+
+        // Convertir entidades anidadas a IDs
+        dto.setIdRubro(entity.getRubro().getId());
+        dto.setIdUnidadMedida(entity.getUnidadMedida().getId());
+
+        // Convertir las listas de entidades anidadas a listas de IDs
+        dto.setDetallesPedidoIds(detallePedidoMapper.toDtoList(entity.getDetallesPedido()).stream().map(DetallePedidoDto::getIdDetallePedido).collect(Collectors.toList()));
+        dto.setProductosBebidasCostoIds(productoBebidaCostoMapper.toDtoList(entity.getProductosBebidasCosto()).stream().map(ProductoBebidaCostoDto::getIdProductoBebidaCosto).collect(Collectors.toList()));
+        dto.setProductosBebidasVentaIds(productoBebidaVentaMapper.toDtoList(entity.getProductosBebidasVenta()).stream().map(ProductoBebidaVentaDto::getIdProductoBebidaVenta).collect(Collectors.toList()));
+        dto.setProductosBebidasStockActualIds(productoBebidaStockActualMapper.toDtoList(entity.getProductosBebidasStockActual()).stream().map(ProductoBebidaStockActualDto::getIdProductoBebidaStockActual).collect(Collectors.toList()));
 
         return dto;
     }
--- src/main/java/com/illuminati/ebs/mapper/GenericMapper.java
+++ src/main/java/com/illuminati/ebs/mapper/GenericMapper.java
@@ -1,9 +1,32 @@
 package com.illuminati.ebs.mapper;
 
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public interface GenericMapper<D, E> {
 
     E toEntity(D dto);
 
     D toDto(E entity);
+
+    default List<E> toEntityList(List<D> dtos) {
+        if (dtos == null) {
+            return null;
+        }
+
+        return dtos.stream()
+                .map(this::toEntity)
+                .collect(Collectors.toList());
+    }
+
+    default List<D> toDtoList(List<E> entities) {
+        if (entities == null) {
+            return null;
+        }
+
+        return entities.stream()
+                .map(this::toDto)
+                .collect(Collectors.toList());
+    }
 }
