47685ee7af53f14ef667784b0851e1e5720daca0
RamaHualpusher <ramiro.hualpa@alumnos.frm.utn.edu.ar>
2024-03-17 12:47:53 -0300

    Método guardarFactura recibiendo Pedido funcionando

--- src/main/java/com/illuminati/ebs/service/FacturaService.java
+++ src/main/java/com/illuminati/ebs/service/FacturaService.java
@@ -1,6 +1,9 @@
 package com.illuminati.ebs.service;
 
 import com.illuminati.ebs.entity.Factura;
+import com.illuminati.ebs.entity.Pedido;
+import com.illuminati.ebs.exception.ServiceException;
 
 public interface FacturaService extends GenericService<Factura,Long>{
+    public Factura saveFactura(Pedido pedido) throws ServiceException;
 }
--- src/main/java/com/illuminati/ebs/service/impl/DetalleFacturaServiceImpl.java
+++ src/main/java/com/illuminati/ebs/service/impl/DetalleFacturaServiceImpl.java
@@ -3,7 +3,9 @@ package com.illuminati.ebs.service.impl;
 import com.illuminati.ebs.entity.DetalleFactura;
 import com.illuminati.ebs.repository.DetalleFacturaRespository;
 import com.illuminati.ebs.service.DetalleFacturaService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class DetalleFacturaServiceImpl  extends GenericServiceImpl<DetalleFactura,Long> implements DetalleFacturaService {
     public DetalleFacturaServiceImpl(DetalleFacturaRespository respository){
         super(respository);
